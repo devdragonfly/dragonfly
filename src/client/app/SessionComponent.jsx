e8b2510344a0cdbfbe1c1c3ec5a9b6950ffbe4ab
Chris Pelling <cppelling@gmail.com>
2017-06-07 22:39:55 +0000

    created stars component for use in sessions

--- src/client/app/SessionComponent.jsx
+++ src/client/app/SessionComponent.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router';
 import OrganizationMenuComponent from './OrganizationMenuComponent.jsx';
-
+import StarsComponent from './StarsComponent.jsx';
 
 
 
@@ -74,6 +74,9 @@ class Question extends React.Component {
   
 
   render() {
+    
+    var stars = function() {return <StarsComponent count="3.5" /> }();
+    
     return (
         <div className="dragon-select-list-row">
           <div className="dragon-select-list-cell">
@@ -88,28 +91,7 @@ class Question extends React.Component {
             00.00s
           </div>
           <div className="dragon-select-list-cell">
-              <select className="form-control input-sm" value={this.state.weight}  onChange={this.updateWeight}>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-                <option value="5">5</option>
-                <option value="6">6</option>
-                <option value="7">7</option>
-                <option value="8">8</option>
-                <option value="9">9</option>
-                <option value="10">10</option>
-                <option value="11">11</option>
-                <option value="12">12</option>
-                <option value="13">13</option>
-                <option value="14">14</option>
-                <option value="15">15</option>
-                <option value="16">16</option>
-                <option value="17">17</option>
-                <option value="18">18</option>
-                <option value="19">19</option>
-                <option value="20">20</option>
-              </select>
+            {stars}
           </div>
         </div>
     );
---
+++ src/client/app/StarsComponent.jsx
@@ -0,0 +1,94 @@
+import React from 'react';
+
+
+class StarsComponent extends React.Component {
+
+  constructor(props) {
+    super(props);
+    var count = props.count;
+    var starsArray = this.getStarsArray(count);
+    this.state = {starsArray: starsArray};
+    this.getStarsArray = this.getStarsArray.bind(this);
+    this.handleStarClicked = this.handleStarClicked.bind(this);
+    
+  }
+
+  render() {
+    
+    var starsArray = this.state.starsArray;
+    var handleStarClicked = this.handleStarClicked;
+    var starsJsx = starsArray.map((starValue, i) => {
+                return <Star star={i} starValue={starValue} handleStarClicked={handleStarClicked} />
+        });
+    
+    return (
+        <div>
+          {starsJsx}
+        </div>
+    );
+  }
+  
+  getStarsArray(count) {
+    var starsArray = [0, 0, 0, 0, 0]
+    if (count >= 0.5) starsArray[0] = 0.5;
+    if (count >= 1.0) starsArray[0] = 1.0;
+    if (count >= 1.5) starsArray[1] = 0.5;
+    if (count >= 2.0) starsArray[1] = 1.0;
+    if (count >= 2.5) starsArray[2] = 0.5;
+    if (count >= 3.0) starsArray[2] = 1.0;
+    if (count >= 3.5) starsArray[3] = 0.5;
+    if (count >= 4.0) starsArray[3] = 1.0;
+    if (count >= 4.5) starsArray[4] = 0.5;
+    if (count >= 5.0) starsArray[4] = 1.0;
+    return starsArray;
+  }
+  
+  handleStarClicked(star) {
+    var starsArray = this.state.starsArray;
+    var prevStarValue = starsArray[star];
+    var newStarValue = 1;
+    if (prevStarValue === 1) newStarValue = 0.5;
+    var count = star + newStarValue;
+    var newStarsArray = this.getStarsArray(count);
+    this.setState({
+      starsArray: newStarsArray
+    });    
+  }
+
+
+}
+
+
+
+
+
+class Star extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    
+    var starValue = this.props.starValue;
+    var starClassName = "fa fa-star-o fa-fw dragonfly-blue";
+    if (starValue === 0.5) starClassName = "fa fa-star-half-o fa-fw dragonfly-blue";
+    if (starValue === 1) starClassName = "fa fa-star fa-fw dragonfly-blue";
+
+    return (
+        <i className={starClassName} onClick={this.handleStarClicked.bind(this, this.props.star)}></i>
+    );
+  }
+
+  handleStarClicked(star) {
+    this.props.handleStarClicked(star);
+  }
+
+}
+
+
+
+
+
+export default StarsComponent;
