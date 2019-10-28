d7ccbd5ce4c8eb0e7fda7009e77faadaff9722c8
Matt Twardowski <mttwardowski@gmail.com>
2019-10-28 19:25:02 -0400

    Removed Dashboard page tab

--- src/client/app/components/base/AppMenuComponent.jsx
+++ src/client/app/components/base/AppMenuComponent.jsx
@@ -34,7 +34,7 @@ class AppMenuComponent extends React.Component {
         <div className="row app-menu justify-content-center">
           <div className="col-12 col-lg-10 app-nav-container">
             <ul className="nav nav-tabs border-0 flex-column flex-lg-row">
-              <li className="nav-item"><Link className={homeClass} to={`campaigns`}><i className="far fa-chart-bar"></i> Dashboard</Link></li>
+              {/* <li className="nav-item"><Link className={homeClass} to={`campaigns`}><i className="far fa-chart-bar"></i> Dashboard</Link></li> */}
               <li className="nav-item"><Link className={campaignsClass} to={`campaigns`}><i className="far fa-paper-plane"></i> Dragonflies</Link></li>
               <li className="nav-item"><Link className={sessionsClass} to={`sessions`}><i className="far fa-play-circle"></i> Builder</Link></li>
               {/* <li className="nav-item"><Link className={videosClass} to={`videos`}><i className="fas fa-video"></i> Videos</Link></li> */}
