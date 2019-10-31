6c8c0f53d4f201c1b737837e04a28c5c10821188
Matt Twardowski <mttwardowski@gmail.com>
2019-10-30 22:21:23 -0400

    Added New Menu Styling to Change Organization Name Page

--- src/client/app/OrganizationNameComponent.jsx
+++ src/client/app/OrganizationNameComponent.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'react-router';
-import OrganizationMenuComponent from './OrganizationMenuComponent.jsx';
+import AppMenuComponent from './components/base/AppMenuComponent.jsx';
 
 
 const buttonClassName = "btn btn-primary";
@@ -9,9 +9,10 @@ class OrganizationNameComponent extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {nameValue : props.organizationName,
-                  buttonRestClassName : buttonClassName,
-                  buttonClickedClassName : "dragon-hidden"
+    this.state = {
+      nameValue: props.organizationName,
+      buttonRestClassName: buttonClassName,
+      buttonClickedClassName: "dragon-hidden"
     };
     this.updateNameValue = this.updateNameValue.bind(this);
     this.showClickedButtonState = this.showClickedButtonState.bind(this);
@@ -19,34 +20,33 @@ class OrganizationNameComponent extends React.Component {
   }
 
   render() {
-    
-    var organizationMenu = function() {return <OrganizationMenuComponent current="settings" /> }();
-    
-    
+
+    var appMenu = function () { return <AppMenuComponent current="settings" /> }();
+
     return (
 
-        <div className="row">
-          {organizationMenu}
-          <div className="col-sm-4">
-          
-          
+      <div id="organization_name_component">
+        {appMenu}
+
+        <div className="row justify-content-center">
+          <div className="col-12 col-lg-10">
+
+
             <form onSubmit={this.handleSubmit}>
-                <h3>Organization Name</h3>
-                
-                <br/><br/>
-                
-                <input value={this.state.nameValue} onChange={this.updateNameValue} className="form-control input-lg" placeholder="name of organization"/>
-                <br/>
+              <h3>Organization Name</h3>
+
+              <br /><br />
+
+              <input value={this.state.nameValue} onChange={this.updateNameValue} className="form-control input-lg" placeholder="name of organization" />
+              <br />
               <input type="submit" className={this.state.buttonRestClassName} value="Save" />
               <div className={this.state.buttonClickedClassName}><i className='fa fa-circle-o-notch fa-spin'></i> Saving</div>
             </form>
-          </div> 
-          
-          
-          
-          <div className="col-sm-4">
           </div>
+
         </div>
+
+      </div>
 
 
 
@@ -58,20 +58,20 @@ class OrganizationNameComponent extends React.Component {
 
   showClickedButtonState(yes) {
     if (yes) {
-          this.setState({ buttonRestClassName: "dragon-hidden" });
-          this.setState({ buttonClickedClassName: buttonClassName });
+      this.setState({ buttonRestClassName: "dragon-hidden" });
+      this.setState({ buttonClickedClassName: buttonClassName });
     } else {
-          this.setState({ buttonRestClassName: buttonClassName });
-          this.setState({ buttonClickedClassName: "dragon-hidden" });
+      this.setState({ buttonRestClassName: buttonClassName });
+      this.setState({ buttonClickedClassName: "dragon-hidden" });
     }
   }
-  
+
   updateNameValue(e) {
     this.setState({
       nameValue: e.target.value
     });
   }
-  
+
   handleSubmit(e) {
     e.preventDefault();
     this.showClickedButtonState(true);
@@ -79,10 +79,10 @@ class OrganizationNameComponent extends React.Component {
     const nameValue = this.state.nameValue.trim();
     const userIdValue = this.props.userId;
     const organizationIdValue = this.props.organizationId;
-    
+
     alert('need to add code to save name');
-    myThis.showClickedButtonState(false); 
-    
+    myThis.showClickedButtonState(false);
+
   }
 
 
