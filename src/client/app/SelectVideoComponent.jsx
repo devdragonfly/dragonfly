ba91a0726fb2229eb44355ef37f9e2d773543212
Matt Twardowski <mttwardowski@gmail.com>
2019-11-11 14:01:40 -0500

    Added New Menu styling to Select Video Page

--- src/client/app/SelectVideoComponent.jsx
+++ src/client/app/SelectVideoComponent.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router';
-import OrganizationMenuComponent from './OrganizationMenuComponent.jsx';
+// import OrganizationMenuComponent from './OrganizationMenuComponent.jsx';
+
+import AppMenuComponent from './components/base/AppMenuComponent.jsx';
+
 
 
 const buttonClassName = "btn btn-primary";
@@ -9,18 +12,19 @@ class SelectVideoComponent extends React.Component {
 
   constructor(props) {
     super(props);
-    
+
     var videoId = props.session.videoId;
 
-    this.state = {videoId: videoId,
-                  buttonRestClassName : buttonClassName,
-                  buttonClickedClassName : "dragon-hidden"
+    this.state = {
+      videoId: videoId,
+      buttonRestClassName: buttonClassName,
+      buttonClickedClassName: "dragon-hidden"
     };
     this.showClickedButtonState = this.showClickedButtonState.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.updateVideoId = this.updateVideoId.bind(this);
   }
-  
+
   componentWillMount() {
     var videos = this.props.videos;
     if (videos === 'not found') {
@@ -28,62 +32,60 @@ class SelectVideoComponent extends React.Component {
       this.props.history.push('loadvideos');
     }
   }
-  
+
 
   render() {
     var videos = this.props.videos;
-    
-    var videoOptions = function() {return '' }();
+
+    var videoOptions = function () { return '' }();
 
     if (videos !== 'not found') {
-          if (videos.length === 0) {
-            videoOptions = function() {return 'No videos uploaded yet.' }();
-            
-          } else {
-            videoOptions = videos.map((video, i) => {
-                return <VideoOption video={video}/>;
-            });
-          }
+      if (videos.length === 0) {
+        videoOptions = function () { return 'No videos uploaded yet.' }();
+
+      } else {
+        videoOptions = videos.map((video, i) => {
+          return <VideoOption video={video} />;
+        });
+      }
     }
 
 
-    
-    var organizationMenu = function() {return <OrganizationMenuComponent current="sessions" /> }();
+    var appMenu = function () { return <AppMenuComponent current="sessions" /> }();
+
 
     return (
 
-        <div className="row">
-          {organizationMenu}
-          
-          
-          
-          <div className="col-sm-4">
-            <form onSubmit={this.handleSubmit}>
-                <h3><i className='fa fa-file-video-o fa-fw'></i> {this.props.session.name}</h3>
-                
-                <br/>
-                
-                Select a video for this session:
-                <br/>
-                
-                <select className="form-control" onChange={this.updateVideoId}>
-                  {videoOptions}
-                </select>
-                
-                <br/>
+      <div id="viewCampaignComponent">
+        {appMenu}
 
-                
+        <div className="row justify-content-center">
+
+          <div className="col-12 col-lg-10">
+
+            <form onSubmit={this.handleSubmit}>
+              <h3><i className='fa fa-file-video-o fa-fw'></i> {this.props.session.name}</h3>
+
+              <br />
+
+              Select a video for this session:
+                <br />
+
+              <select className="form-control" onChange={this.updateVideoId}>
+                {videoOptions}
+              </select>
+
+              <br />
+
+
               <input type="submit" className={this.state.buttonRestClassName} value="Save" />
               <div className={this.state.buttonClickedClassName}><i className='fa fa-circle-o-notch fa-spin'></i> Saving</div>
             </form>
-            
-          </div> 
-          
-          
-          <div className="col-sm-6">
-          </div>
 
+          </div>
+          
         </div>
+      </div>
 
 
 
@@ -92,53 +94,53 @@ class SelectVideoComponent extends React.Component {
 
   showClickedButtonState(yes) {
     if (yes) {
-          this.setState({ buttonRestClassName: "dragon-hidden" });
-          this.setState({ buttonClickedClassName: buttonClassName });
+      this.setState({ buttonRestClassName: "dragon-hidden" });
+      this.setState({ buttonClickedClassName: buttonClassName });
     } else {
-          this.setState({ buttonRestClassName: buttonClassName });
-          this.setState({ buttonClickedClassName: "dragon-hidden" });
+      this.setState({ buttonRestClassName: buttonClassName });
+      this.setState({ buttonClickedClassName: "dragon-hidden" });
     }
   }
-  
-  
+
+
   updateVideoId(e) {
     this.setState({
       videoId: e.target.value
     });
   }
-  
+
 
   handleSubmit(e) {
     e.preventDefault();
     this.showClickedButtonState(true);
     var myThis = this;
-    
+
     const organizationId = this.props.organizationId;
     const sessionId = this.props.session.sessionId;
     var session = this.props.session;
     var videos = this.props.videos;
     var videoId = this.state.videoId;
     var video = "not found";
-    
-    for (var i = 0; i < videos.length; i++) {
-          if(videos[i].videoId === videoId) { video = videos[i] }
-    }
-   
-    var params = {
-            TableName:"Sessions",
-            Key: {
-                organizationId : organizationId,
-                sessionId : sessionId
-            },
-            UpdateExpression: "set video = :video",
-            ExpressionAttributeValues:{
-                ":video" : video
-            },
-            ReturnValues:"UPDATED_NEW"
-        };
-    
 
-    this.props.dbUpdate(params, function(result) {
+    for (var i = 0; i < videos.length; i++) {
+      if (videos[i].videoId === videoId) { video = videos[i] }
+    }
+
+    var params = {
+      TableName: "Sessions",
+      Key: {
+        organizationId: organizationId,
+        sessionId: sessionId
+      },
+      UpdateExpression: "set video = :video",
+      ExpressionAttributeValues: {
+        ":video": video
+      },
+      ReturnValues: "UPDATED_NEW"
+    };
+
+
+    this.props.dbUpdate(params, function (result) {
       myThis.showClickedButtonState(false);
       session.video = video;
       session.thumbnailState = "unknown";
@@ -146,7 +148,7 @@ class SelectVideoComponent extends React.Component {
       myThis.props.handleLoadVideo(video);
       myThis.props.history.push('session');
     });
-    
+
   }
 
 }
